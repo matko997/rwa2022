41b806b84ac2fd7a74c18f98f3af81ed3617f253
matko <matko.dugandzic@fsre.sum.ba>
2022-09-06 01:13:29 +0200

    .

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,9 +26,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-//        Schema::defaultStringLength(191);
-//        if (isset($_SERVER['HTTPS']) && ($_SERVER['HTTPS'] == 'on' || $_SERVER['HTTPS'] == 1) || isset($_SERVER['HTTP_X_FORWARDED_PROTO']) &&  $_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https') {
-//            URL::forceScheme('https');
-//        }
+        Schema::defaultStringLength(191);
     }
 }
